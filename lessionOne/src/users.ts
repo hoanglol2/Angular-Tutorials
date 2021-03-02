d6e7058b2beae1d52c82f756a505dbf2861f5c48
hoanglol2 <duyhoangpt98@gmail.com>
2021-03-02 11:03:05 +0700

    type script

--- lessionOne/src/users.ts
+++ lessionOne/src/users.ts
@@ -1,21 +1,27 @@
-type User = {
-  firstName: string;
-  job?: string;
+// tslint:disable-next-line:typedef
+// Union Type - Pipe Symbol (|) (hoac la cai nay hoac la cai kia)
+function listen(port: string | number) {
+//    do something
 };
 
-interface User1 {
-  firstName: string;
-  lastName: number;
-  job?: string;
-}
+listen('3000');
+listen(3000);
+listen(true);
 
-// job -> Optional Property (thuộc tính tùy chọn)
-const susan: User = {
-  firstName: 'Hoang',
+
+// Union Type cho gia tri tra ve cua 1 ham
+function getSomeThing(): string | number {
+  // return '12';
+  return 1;
 };
 
-const asia: User1 = {
-  firstName: 'sda',
-  lastName: 0,
+// Intersection Type - ket hop nhieu loai type voi nhau (AND)
+//  được dùng rất nhiều trong việc thiết kế hệ thống type cho những thư viện UI Components
+// marge type.
+type StyleProp = {
+  backgroundColor: string;
 };
 
+type ButtonProps = {
+  onClick: (event: MouseEvent) => void;
+} & StyleProp;
